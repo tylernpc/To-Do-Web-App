ba60bfcdc1b80238372c1680c2962bb8804711fb
tylernpc <103677076+tylernpc@users.noreply.github.com>
2025-01-20 20:37:27 -0600

    Updates Complete

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1329bd180a2a0d585d92bc7ff835138caa4dda9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d34d5c013295b48d74cc32bcc6d05af0fea37acd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
